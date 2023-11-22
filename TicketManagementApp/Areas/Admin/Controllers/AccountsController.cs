1f8d35a2b1980283609c672bc4f000e147183c2d
Mohammad Amin Arjang <m-arjang@kms.net>
2023-11-22 09:02:13 +0330

    commit

---
+++ TicketManagementApp/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagementApp.Context;
+using TicketManagementApp.Models;
+
+namespace TicketManagementApp.Areas.Admin.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private TkContext db = new TkContext();
+
+        // GET: Admin/Accounts
+        public ActionResult Index()
+        {
+            var accounts = db.Accounts.Include(a => a.UserGroup);
+            return View(accounts.ToList());
+        }
+
+        // GET: Admin/Accounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accounts accounts = db.Accounts.Find(id);
+            if (accounts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accounts);
+        }
+
+        // GET: Admin/Accounts/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserGroupID = new SelectList(db.UserGroups, "UserGroupID", "UserGroupTitle");
+            return View();
+        }
+
+        // POST: Admin/Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountID,UserGroupID,Username,Password,FullName")] Accounts accounts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(accounts);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserGroupID = new SelectList(db.UserGroups, "UserGroupID", "UserGroupTitle", accounts.UserGroupID);
+            return View(accounts);
+        }
+
+        // GET: Admin/Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accounts accounts = db.Accounts.Find(id);
+            if (accounts == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserGroupID = new SelectList(db.UserGroups, "UserGroupID", "UserGroupTitle", accounts.UserGroupID);
+            return View(accounts);
+        }
+
+        // POST: Admin/Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountID,UserGroupID,Username,Password,FullName")] Accounts accounts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accounts).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserGroupID = new SelectList(db.UserGroups, "UserGroupID", "UserGroupTitle", accounts.UserGroupID);
+            return View(accounts);
+        }
+
+        // GET: Admin/Accounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accounts accounts = db.Accounts.Find(id);
+            if (accounts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accounts);
+        }
+
+        // POST: Admin/Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Accounts accounts = db.Accounts.Find(id);
+            db.Accounts.Remove(accounts);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
